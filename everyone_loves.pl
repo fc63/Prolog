33fd90a23097b25849a34a6e520613dcd7400508
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2025-01-06 13:55:07 +0300

    Update everyone_loves.pl
    x'in person olduğunu bir kez kontrol etmemiz yeterli

--- everyone_loves.pl
+++ everyone_loves.pl
@@ -1,5 +1,5 @@
-everyone_loves(X) :- person(X), forall(person(Y), love(Y, X)).
+everyone_loves(X) :- forall(person(Y), love(Y, X)).
 
-not_self_love(X) :- person(X), \+ love(X, X).
+not_self_love(X) :- \+ love(X, X).
 
 everyone_loves_but_not_self(X) :- person(X), everyone_loves(X), not_self_love(X).
